b2a83f7f2e62ab7f865ecc2a5d6f7dd72641a335
reza2c <107634689+reza2c@users.noreply.github.com>
2024-11-28 14:12:18 +0330

    hemixyz.sh

--- hemixyz.sh
+++ hemixyz.sh
@@ -20,58 +20,42 @@ if ! command -v jq &> /dev/null; then
     fi
 fi
 
-
-check_latest_version() {
-    for i in {1..3}; do
-        LATEST_VERSION=$(curl -s https://api.github.com/repos/hemilabs/heminetwork/releases/latest | jq -r '.tag_name')
-        if [ -n "$LATEST_VERSION" ]; then
-            show "Latest version available: $LATEST_VERSION"
-            return 0
-        fi
-        show "Attempt $i: Failed to fetch the latest version. Retrying..."
-        sleep 2
-    done
-
-    show "Failed to fetch the latest version after 3 attempts. Please check your internet connection or GitHub API limits."
-    exit 1
-}
-
-check_latest_version
-
+# Hardcoded version
+VERSION="v0.5.0"
 
 download_required=true
 
 if [ "$ARCH" == "x86_64" ]; then
-    if [ -d "heminetwork_${LATEST_VERSION}_linux_amd64" ]; then
-        show "Latest version for x86_64 is already downloaded. Skipping download."
-        cd "heminetwork_${LATEST_VERSION}_linux_amd64" || { show "Failed to change directory."; exit 1; }
+    if [ -d "heminetwork_${VERSION}_linux_amd64" ]; then
+        show "Version $VERSION for x86_64 is already downloaded. Skipping download."
+        cd "heminetwork_${VERSION}_linux_amd64" || { show "Failed to change directory."; exit 1; }
         download_required=false  # Set flag to false
     fi
 elif [ "$ARCH" == "arm64" ]; then
-    if [ -d "heminetwork_${LATEST_VERSION}_linux_arm64" ]; then
-        show "Latest version for arm64 is already downloaded. Skipping download."
-        cd "heminetwork_${LATEST_VERSION}_linux_arm64" || { show "Failed to change directory."; exit 1; }
+    if [ -d "heminetwork_${VERSION}_linux_arm64" ]; then
+        show "Version $VERSION for arm64 is already downloaded. Skipping download."
+        cd "heminetwork_${VERSION}_linux_arm64" || { show "Failed to change directory."; exit 1; }
         download_required=false  # Set flag to false
     fi
 fi
 
 if [ "$download_required" = true ]; then
     if [ "$ARCH" == "x86_64" ]; then
-        show "Downloading for x86_64 architecture..."
-        wget --quiet --show-progress "https://github.com/hemilabs/heminetwork/releases/download/v0.5.0/heminetwork_v0.5.0_linux_amd64.tar.gz" -O "heminetwork_v0.5.0_linux_amd64.tar.gz"
-        tar -xzf "heminetwork_v0.5.0_linux_amd64.tar.gz" > /dev/null
-        cd "heminetwork_v0.5.0_linux_amd64" || { show "Failed to change directory."; exit 1; }
+        show "Downloading version $VERSION for x86_64 architecture..."
+        wget --quiet --show-progress "https://github.com/hemilabs/heminetwork/releases/download/$VERSION/heminetwork_${VERSION}_linux_amd64.tar.gz" -O "heminetwork_${VERSION}_linux_amd64.tar.gz"
+        tar -xzf "heminetwork_${VERSION}_linux_amd64.tar.gz" > /dev/null
+        cd "heminetwork_${VERSION}_linux_amd64" || { show "Failed to change directory."; exit 1; }
     elif [ "$ARCH" == "arm64" ]; then
-        show "Downloading for arm64 architecture..."
-        wget --quiet --show-progress "https://github.com/hemilabs/heminetwork/releases/download/v0.5.0/heminetwork_v0.5.0_linux_arm64.tar.gz" -O "heminetwork_v0.5.0_linux_arm64.tar.gz"
-        tar -xzf "heminetwork_v0.5.0_linux_arm64.tar.gz" > /dev/null
-        cd "heminetwork_v0.5.0_linux_arm64" || { show "Failed to change directory."; exit 1; }
+        show "Downloading version $VERSION for arm64 architecture..."
+        wget --quiet --show-progress "https://github.com/hemilabs/heminetwork/releases/download/$VERSION/heminetwork_${VERSION}_linux_arm64.tar.gz" -O "heminetwork_${VERSION}_linux_arm64.tar.gz"
+        tar -xzf "heminetwork_${VERSION}_linux_arm64.tar.gz" > /dev/null
+        cd "heminetwork_${VERSION}_linux_arm64" || { show "Failed to change directory."; exit 1; }
     else
         show "Unsupported architecture: $ARCH"
         exit 1
     fi
 else
-    show "Skipping download as the latest version is already present."
+    show "Skipping download as version $VERSION is already present."
 fi
 
 echo
@@ -80,7 +64,6 @@ show "1. Use new wallet for PoP mining"
 show "2. Use existing wallet for PoP mining"
 read -p "Enter your choice (1/2): " choice
 echo
-
 
 if [ "$choice" == "1" ]; then
     show "Generating a new wallet..."
@@ -111,7 +94,6 @@ elif [ "$choice" == "2" ]; then
     read -p "Enter static fee (numerical only, recommended: 100-200): " static_fee
     echo
 fi
-
 
 if systemctl is-active --quiet hemi.service; then
     show "hemi.service is currently running. Stopping and disabling it..."
@@ -144,4 +126,4 @@ sleep 15
 sudo systemctl enable hemi.service
 sudo systemctl start hemi.service
 echo
-show "PoP mining is successfully srtated"
+show "PoP mining is successfully started"
